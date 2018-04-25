5acdee40d9afcb1d62ca331ec0fcf00f815856d2
Son <sonpd@avr-solutions.com>
2018-04-26 06:06:51 +0700

    Spawner

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour {
 
 	public float rate;
-	public FakeGravity fakeGrav;
+	public Transform anchor;
+	public Transform center;
 
 	// Use this for initialization
 	void Start () {
-		fakeGrav = this.GetComponent<FakeGravity> ();
+
 		StartCoroutine (Spawn());
 	}
 
@@ -19,17 +20,27 @@ public class EnemySpawner : MonoBehaviour {
 			yield return new WaitForSeconds (rate);
 			Enemy enemyGo = ObjectPool.instance.GetEnemy ();
 			enemyGo.Live ();
-			Vector3 pos = Random.insideUnitSphere * 5.5f;
+			Vector3 pos = RandomPoint ();
 			enemyGo.transform.position = pos;
 			Rigidbody rb = enemyGo.GetComponent<Rigidbody> ();
 			rb.velocity = Vector3.zero;
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
 
+	float distanceToCenter;
+
+	Vector3 RandomPoint()
+	{
+		float distanceToCenter = Vector3.Distance (anchor.position,center.position);
+
+		Vector3 pos = Random.insideUnitSphere * 5.5f;
+
+		while (Vector3.Distance (pos, center.position) < distanceToCenter) {
+			pos = Random.insideUnitSphere * 5.5f;
+		}
+		return pos;
+
+	}
 
 
 
